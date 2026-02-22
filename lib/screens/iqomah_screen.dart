af67c96c6120de0d5a8c08f4e0f783cc9d970ab1
Feri Setiyawan <feri.setiyawan@akarinti.tech>
2026-02-22 18:14:52 +0700

    change color iqomah below 10 to red

--- lib/screens/iqomah_screen.dart
+++ lib/screens/iqomah_screen.dart
@@ -16,8 +16,10 @@ class IqomahScreen extends StatelessWidget {
     String minutes = (countdown ~/ 60).toString().padLeft(2, '0');
     String seconds = (countdown % 60).toString().padLeft(2, '0');
 
+    Color timerColor = (countdown <= 10) ? Colors.redAccent : Colors.greenAccent;
+
     return Scaffold(
-      backgroundColor: Colors.transparent, // Agar background wrapper kelihatan
+      backgroundColor: Colors.transparent,
       body: Center(
         child: BackdropFilter(
           filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
@@ -30,24 +32,29 @@ class IqomahScreen extends StatelessWidget {
               border: Border.all(color: Colors.white.withValues(alpha: 0.1)),
             ),
             child: Column(
-              mainAxisSize: MainAxisSize.min, // Sesuai isi konten
+              mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text("MENUJU IQOMAH", style: TextStyle(fontSize: 35, fontWeight: FontWeight.w300, letterSpacing: 8)),
-                Text(namaSholat.toUpperCase(), style: const TextStyle(fontSize: 25, color: Colors.white70)),
+                const Text(
+                  "MENUJU IQOMAH", 
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.w300, letterSpacing: 8)
+                ),
+                Text(
+                  namaSholat.toUpperCase(), 
+                  style: const TextStyle(fontSize: 25, color: Colors.white70)
+                ),
                 
-                // Menggunakan FittedBox agar teks jam tidak overflow
                 Flexible(
                   child: FittedBox(
                     fit: BoxFit.scaleDown,
                     child: Text(
                       "$minutes:$seconds",
-                      style: const TextStyle(
-                        fontSize: 250, // Ukuran target
+                      style: TextStyle(
+                        fontSize: 250, 
                         fontWeight: FontWeight.w900, 
-                        color: Colors.greenAccent, 
-                        fontFeatures: [FontFeature.tabularFigures()],
-                        height: 1.1, // Mengurangi spasi vertikal teks
+                        color: timerColor,
+                        fontFeatures: const [FontFeature.tabularFigures()],
+                        height: 1.1,
                       ),
                     ),
                   ),
